4d394c0482a2b7498e7cea4c8a4bf96943c9f2ff
Rajeev Ranjan <rajeev.ranjan8284@gmail.com>
2023-10-11 10:31:48 +0530

    Menu files

---
+++ src/app/layout/app.topbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { LayoutService } from "./service/app.layout.service";
+import { Router} from '@angular/router';
+import { OverlayPanel } from 'primeng/overlaypanel';
+
+@Component({
+    selector: 'app-topbar',
+    templateUrl: './app.topbar.component.html'
+})
+export class AppTopBarComponent {
+
+    items!: MenuItem[];
+
+    @ViewChild('menubutton') menuButton!: ElementRef;
+
+    @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
+
+    @ViewChild('topbarmenu') menu!: ElementRef;
+
+    constructor(
+        public layoutService: LayoutService,
+        private router: Router 
+        ) { }
+
+    logout(){
+        this.router.navigate(['/login'])
+    }
+}
